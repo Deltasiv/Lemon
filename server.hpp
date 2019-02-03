23db47915309dd44a1d7efcf63279f7eef75e43f
Haunting Spectre <43715379+YELLOWHATT@users.noreply.github.com>
2019-02-03 16:49:12 -0700

    Add files via upload

---
+++ server.hpp
@@ -0,0 +1,23 @@
+#ifndef CLEINT_HPP_
+#define CLIENT_HPP_
+
+#include <iostream>
+#include <chrono>
+#include <sstream>
+#include <fstream>
+#include <thread>
+#include <string>
+#include <cmath>
+
+// server file
+
+int ipv6(void)
+{
+
+	std::cout << "\nTest server\n";
+
+	return (0);
+
+}
+
+#endif
