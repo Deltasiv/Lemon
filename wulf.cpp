73a075137873cd47b5099cce8fafb2afccf93ad0
Haunting Spectre <43715379+YELLOWHATT@users.noreply.github.com>
2019-02-06 00:40:24 -0700

    Add files via upload

--- wulf.cpp
+++ wulf.cpp
@@ -5,36 +5,36 @@
 #include <chrono>
 #include "gidnc.hpp"
 
-std::string foobar = {"\033c"}; 
-
 // FSTREAM will be used for gyroscopic temperature barometric speedometric and altimetric measurements
-// GIDNC = Guidance and Internal Distributed Navigation Computer
-// A temp password will be made for this time 
+// GIDNC == Guidance and Internal Distributed Navigation Computer
+// A temp password will be made for this time
+// New password can be expected
 
-std::string exiting = {"EXITING..."}; 
+std::string foobar = {"\033c"}; // clear screen 
+std::string exiting = {"EXITING..."}; // status exiting
 
-struct errors // Struct for errors
+class control 
 {
 
 	public:
 
-		enum e_code
+		struct errors // Struct for errors
 		{
+
+			public:
+
+				enum e_code // enum for errors which are represented by numbers
+				{
 		
-			ERRORNO4 = 4, // Error for incorrect input
-			ERRORNO1 = 1, // Error for internal issue
-			ERRORNO2 = 2, // Error for incorrect password 
-			ERRORNO3 = 3 // Error for unknown issue
-			// More errors might be included later
+					ERRORNO4 = 4, // Error for incorrect input
+					ERRORNO1 = 1, // Error for internal issue
+					ERRORNO2 = 2, // Error for incorrect password 
+					ERRORNO3 = 3 // Error for unknown issue
+					// More errors might be included later
+			
+				};
+
 		}; 
-
-};
-errors error;
-
-class control
-{
-	
-	public:
 
 		struct light
 		{
@@ -44,8 +44,9 @@ class control
 				char licht[3][25] = {"RED LIGHT", "YELLOW LIGHT", "GREEN LIGHT"};
 
 		}; 	
-
-		light lights;
+		
+		light lights; // The struct above
+		errors mistake; // The struct for error values
 
 		int initialize(void)
 		{
@@ -53,6 +54,7 @@ class control
 			std::cout << (foobar);
 			std::cout << ("Welcome to the GIDNC\n");
 			klar();
+		
 			return (0);
 
 		}
@@ -62,29 +64,31 @@ class control
 int main(void)
 {
 
+	control wulf_start;
+	control errors;
+
 	std::string temp_pass = {"WULFZITRONE"}; // Temporary password
 	std::string pass; // string for entering temporary password
 	std::string errorgen = {"ERROR "}; // General ERROR notation
 
-	error.ERRORNO4; // ERROR for incorrect input
-	error.ERRORNO2; // ERROR for incorrect password 
-	error.ERRORNO1; // Error for internal issue
-	error.ERRORNO3; // Error for unknown issue
+	wulf_start.mistake.ERRORNO4; // ERROR for incorrect input
+	wulf_start.mistake.ERRORNO2; // ERROR for incorrect password 
+	wulf_start.mistake.ERRORNO1; // Error for internal issue
+	wulf_start.mistake.ERRORNO3; // Error for unknown issue
 
 	std::ostringstream(s7); 
-	s7 << errorgen << error.ERRORNO2; 
+	s7 << errorgen << wulf_start.mistake.ERRORNO2; 
 	std::string s9error = s7.str(); 
 
 	std::ostringstream(s1);
-	s1 << errorgen << error.ERRORNO4;
+	s1 << errorgen << wulf_start.mistake.ERRORNO4;
 	std::string s2error = s1.str();
 
 	std::string launch;
 
 	control light;
-	control wulf_start;
 	
-	std::cout << ("Hello I'm ") << ("\u03BD\u03C5");
+	std::cout << ("Hello I'm ") << ("\u03BD\u03C5"); // Program name is nu
 	std::cout << ("\n\nEnter the password to start the program\n\n");	
 	std::cin >> pass;
 	std::cout << (foobar);
